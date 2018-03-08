e72633e06fbb03f22671c1d395bccc882c6a5e34
ZairaRiv <zairariv@mail.fresnostate.edu>
2018-03-07 23:03:16 -0800

    updated rape, flood, and stalking buttons and updated button class to center

--- src/app/components/rape/rape.component.ts
+++ src/app/components/rape/rape.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rape',
+  selector: 'app-gta',
   templateUrl: './rape.component.html',
   styleUrls: ['./rape.component.css']
 })
 export class RapeComponent implements OnInit {
 
   constructor() { }
+  private currentStep = 1;
+  private previousStep = 0;
 
   ngOnInit() {
+  }
+
+  gotoStep(nextstep) {
+    this.previousStep = this.currentStep;
+    this.currentStep = nextstep;
+  }
+
+  getStep()  {
+    return this.currentStep;
   }
 
 }
