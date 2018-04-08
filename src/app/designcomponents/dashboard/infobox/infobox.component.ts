03b1fba5f81567895cef779261363d0c361038ef
Agustin Rivera <agustin@rivera-web.com>
2018-04-08 11:36:59 -0700

    Admin dashboard v1 done

--- src/app/designcomponents/dashboard/infobox/infobox.component.ts
+++ src/app/designcomponents/dashboard/infobox/infobox.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../../../services/data.service';
 import { Observable } from 'rxjs/Observable';
+import { timer } from 'rxjs/observable/timer';
+import { take, count } from 'rxjs/operators';
+import { range } from 'rxjs/observable/range';
+
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/map';
@@ -14,31 +18,42 @@ export class InfoboxComponent implements OnInit {
 
   constructor(private dataService: DataService) { }
 
-  public totalStudents: any;
+  public obj = {
+    totalStudents: 0
+  };
+
+  public totalStudents = 0;
   public totalSchools: number;
   public massShootings: number;
   public massDeaths: number;
   public injuries: number;
-  public timeToCount = 10000; // ms
+  public threatsPerDay: number; // goo.gl/JPRLz1
+
+
+  public timeToCount = 1000; // ms
 
   ngOnInit() {
     this.getUserCount();
     this.getSchoolCount();
+    this.getMassShootings();
+    this.getMassDeaths();
+    this.getInjuries();
+    this.getThreatsPerDay();
   }
 
   getUserCount() {
     this.dataService.getUserCount().subscribe(data => {
       if ((data as any)) {
-        /**
-        console.log(this.timeToCount / data[0].total);
-        const counter = data[0].total;
-        const tick = this.timeToCount / data[0].total;
 
-        this.totalStudents = Observable.timer(0, tick)
-        .take(counter)
-        .map(() => this.totalStudents++);
-        console.log(this.totalStudents);*/
-        this.totalStudents = data[0].total;
+        const tick = this.timeToCount / data[0].total;
+        let total: number;
+        total = data[0].total as number;
+        total++;
+
+        const source = range(1, total);
+        const interval = timer(0, tick);
+        const countDown = interval.pipe(take(total));
+        const subscribe = countDown.subscribe(val => this.totalStudents = val);
       }
     });
   }
@@ -46,15 +61,64 @@ export class InfoboxComponent implements OnInit {
   getSchoolCount() {
     this.dataService.getSchoolCount().subscribe(data => {
       if ((data as any)) {
-        this.totalSchools = data[0].total;
+        const tick = this.timeToCount / data[0].total;
+        let total: number;
+        total = data[0].total as number;
+        total++;
+
+        const source = range(1, total);
+        const interval = timer(0, tick);
+        const countDown = interval.pipe(take(total));
+        const subscribe = countDown.subscribe(val => this.totalSchools = val);
       }
     });
   }
 
-  sleep(i, time) {
-    setTimeout( () => {
-      i++;
-      return i;
-    }, time );
+  getMassShootings() {
+    const tick = this.timeToCount / 4;
+    let total: number;
+    total = 4 as number;
+    total++;
+
+    const source = range(1, total);
+    const interval = timer(0, tick);
+    const countDown = interval.pipe(take(total));
+    const subscribe = countDown.subscribe(val => this.massShootings = val);
+  }
+
+  getMassDeaths() {
+    const tick = this.timeToCount / 25;
+    let total: number;
+    total = 25 as number;
+    total++;
+
+    const source = range(1, total);
+    const interval = timer(0, tick);
+    const countDown = interval.pipe(take(total));
+    const subscribe = countDown.subscribe(val => this.massDeaths = val);
+  }
+
+  getInjuries() {
+    const tick = this.timeToCount / 34;
+    let total: number;
+    total = 34 as number;
+    total++;
+
+    const source = range(1, total);
+    const interval = timer(0, tick);
+    const countDown = interval.pipe(take(total));
+    const subscribe = countDown.subscribe(val => this.injuries = val);
+  }
+
+  getThreatsPerDay() {
+    const tick = this.timeToCount / 70;
+    let total: number;
+    total = 70 as number;
+    total++;
+
+    const source = range(1, total);
+    const interval = timer(0, tick);
+    const countDown = interval.pipe(take(total));
+    const subscribe = countDown.subscribe(val => this.threatsPerDay = val);
   }
 }
